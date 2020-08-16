32a0c5345d88aa20154d792bf5669d66700b7cd7
Omar Shafei <omarshafhy.os@gmail.com>
2020-08-16 15:11:59 +0200

    Initial commit

---
+++ app/src/main/java/com/omarshafei/powerreceiver/CustomReceiver.java
@@ -0,0 +1,28 @@
+package com.omarshafei.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Get the Intent action from the intent parameter and store it in a String variable called intentAction.
+        String intentAction = intent.getAction();
+        String toastMessage = "unknown intent action";
+        //do a null check
+        if(intentAction != null){
+
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected"; break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected"; break;
+                default: break;
+            }
+        }
+        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/omarshafei/powerreceiver/MainActivity.java
@@ -0,0 +1,30 @@
+package com.omarshafei.powerreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        // Register the receiver using the activity context.
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+}
